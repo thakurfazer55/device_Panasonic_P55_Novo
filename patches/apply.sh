30ec4c4684b56e868892bb2cf413267fecb2f0bd
Monty Kumar <aryanmk.mk5@gmail.com>
2017-05-26 23:14:17 -0700

    Fixed paths Typo

--- patches/apply.sh
+++ patches/apply.sh
@@ -4,7 +4,7 @@
  patch -p1 < ../../device/Xiaomi/hm_note_1w/patches/core.patch
  cd ../..
  cd packages/apps/Settings
- patch -p1 < ../../device/Xiaomi/hm_note_1w/patches/author_info.patch
+ patch -p1 < ../../../device/Xiaomi/hm_note_1w/patches/author_info.patch
  cd ../../..
  cd system*/bt
  patch -p1 < ../../device/Xiaomi/hm_note_1w/patches/bt.patch
@@ -29,7 +29,7 @@
  patch -p1 < ../../device/Xiaomi/hm_note_1w/patches/native.patch
  cd ..
  cd opt/te*/
- patch -p1 < ../../device/Xiaomi/hm_note_1w/patches/telephony.patch
+ patch -p1 < ../../../device/Xiaomi/hm_note_1w/patches/telephony.patch
  cd ../../..
  cd exte*/sepolicy
  patch -p1 < ../../device/Xiaomi/hm_note_1w/patches/sepolicy.patch
