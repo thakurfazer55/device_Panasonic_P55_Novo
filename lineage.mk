0b567b9a7a932e17eb13a418181423fbf57358e4
brownietheboy <brownietheboy@gmail.com>
2017-05-31 21:45:00 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := hm_note_1w
+PRODUCT_RELEASE_NAME := P55_Novo
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -10,8 +10,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/Xiaomi/hm_note_1w/device_hm_note_1w.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := hm_note_1w
-PRODUCT_NAME := lineage_hm_note_1w
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Note 3G
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_DEVICE := P55_Novo
+PRODUCT_NAME := lineage_P55_Novo
+PRODUCT_BRAND := Panasonic
+PRODUCT_MODEL := P55 Novo
+PRODUCT_MANUFACTURER := Panasonic
