fff78da66afdcddc3c9da7f1dbaa789e416292c4
brownietheboy <brownietheboy@gmail.com>
2017-05-31 21:38:13 +0530

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/Xiaomi/hm_note_1w/AndroidBoardVendor.mk
+-include vendor/Panasonic/P55_Novo/AndroidBoardVendor.mk
 
 # copy keylayout overrides
 $(shell mkdir -p $(TARGET_OUT_KEYLAYOUT); \
-    cp -pf device/Xiaomi/hm_note_1w/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
+    cp -pf device/Panasonic/P55_Novo/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
